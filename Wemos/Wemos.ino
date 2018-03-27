cdc9348e0f92c2f52242fa64f82b87a2ec308b23
Lybrica <karl.miikael100@gmail.com>
2018-03-27 21:23:34 +0300

    Added seconds

--- Wemos/Wemos.ino
+++ Wemos/Wemos.ino
@@ -15,12 +15,11 @@ const char* WIFI_PASSWORD = "miksonvaja";
 
 // Setup
 const int UPDATE_INTERVAL_inSECS = 10 * 60; // Update every 10 minutes
-const int CLOCK_UPDATE_INTERVAL = 60;
-const float UTC_OFFSET = 2;                 // TimeClient settings
+const float UTC_OFFSET = 3;                 // TimeClient settings
 const String WG_API_KEY = "6fc2508ebadf7b1c";
 const String WG_LANGUAGE = "EN";
 const String WG_COUNTRY = "EE";
-const String WG_CITY = "Tallinn";
+const String WG_CITY = "Viimsi";
 const String WG_ZMW_CODE = "00000.55.26038";
 
 WundergroundConditions wgc(true);                // True = use metric
@@ -28,7 +27,7 @@ WundergroundClient wg(true);
 
 bool readyForWUpdate = false;
 bool readyForCUpdate = true;
-String dots;
+bool readyforConnect = false;
 String lastUpdate = "-";
 String months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 String dayIcons[] = {"chanceflurries", "chancerain", "chancesleet", "chancesnow", "chancetstorms", "clear", "cloudy", "flurries", "fog", "hazy", "mostlycloudy", "mostlysunny", "nt_chanceflurries", "nt_chancerain", "nt_chancesleet", "nt_chancesnow", "nt_chancetstorms", "nt_clear", "nt_cloudy", "nt_flurries", "nt_fog", "nt_hazy", "nt_mostlycloudy", "nt_mostlysunny", "nt_partlycloudy", "nt_partlysunny", "nt_rain", "nt_sleet", "nt_snow", "nt_sunny", "nt_tstorms", "nt_unknown", "partlycloudy", "partlysunny", "rain", "sleet", "snow", "sunny", "tstorms", "unknown"};
@@ -40,6 +39,7 @@ NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600 * UTC_OFFSET, 60000);
 
 
 Ticker ticker;
+Ticker ticker1;
 Nextion nex(nextion, 9600); //create a Nextion object named myNextion using the nextion serial port @ 9600bps
 
 /*===============================================================================
@@ -54,6 +54,7 @@ void connectWifi() {
   Serial.println(WIFI_SSID);
   nex.setComponentText("textLoading", "Connecting to WiFi");
   int tim = 0;
+  String dots;
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -84,25 +85,60 @@ void updateData() {
   Serial.println("\n\nUpdating conditions...");
   WGConditions conditions;
   wgc.updateConditions(&conditions, WG_API_KEY, WG_LANGUAGE, WG_ZMW_CODE);
-  for (int i = 0; i < sizeof(icons) / sizeof(icons[0]); i++) {            //Loop through Icons array
-      if (conditions.weatherIcon == icons[i] || icons_alt[i]) {
-        String IconFinal = "p0.pic=" + String(i);  //forecast icons start from id 15 (on nextion)
-        char *pointerString = new char[  IconFinal.length() + 1];                //Covert string to *char
-        strcpy(pointerString, IconFinal.c_str());                              //
-        nex.sendCommand(pointerString);                                           //Change the icon on screen
-        delete [] pointerString;                                                  //Delete the *char to free up mem
-        IconFinal = "NexTemp.picc=" + String(i);
-        char *pointerStr = new char[  IconFinal.length() + 1];
-        strcpy(pointerStr, IconFinal.c_str());
-        nex.sendCommand(pointerStr);
-        delete [] pointerStr;
-        break;
-      }
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("\nNo connection");
+    nex.sendCommand("p0.pic=14");                                           //Set the icon to 'unknown'    
+    nex.sendCommand("NexTemp.picc=14");
+    nex.sendCommand("NexTimeSec.picc=14");
+    nex.setComponentText("NexTemp", "");
+    nex.setComponentText("NexHumid", "?");
+    nex.setComponentText("NexFLike", "?");
+    nex.setComponentText("NexWSpeed", "?");
+    for (int i=0; i < 3; i++) {
+      String dayIconErased = "p" + String(i + 1) + ".pic=55";
+      char *pointerString = new char[dayIconErased.length() + 1];
+      strcpy(pointerString, dayIconErased.c_str()); 
+      nex.sendCommand(pointerString);
+      delete [] pointerString;
+      nex.setComponentText("tagDay" + String(i + 1), "");
+      nex.setComponentText("tagNight" + String(i + 1), "");
+      nex.setComponentText("NexTemp" + String(i + 1), "");
     }
+    readyforConnect = true;
+  }
+  else
+  {
+    WGConditions conditions;
+    wgc.updateConditions(&conditions, WG_API_KEY, WG_LANGUAGE, WG_ZMW_CODE);
+    for (int i = 0; i < sizeof(icons) / sizeof(icons[0]); i++) {            //Loop through Icons array
+        if (conditions.weatherIcon == icons[i] || conditions.weatherIcon == icons_alt[i]) {
+          Serial.println("Icon index: " + String(i) + " | Icon: " + conditions.weatherIcon);
+          String IconFinal = "p0.pic=" + String(i);  //forecast icons start from id 15 (on nextion)
+          char *pointerString = new char[  IconFinal.length() + 1];                //Covert string to *char
+          strcpy(pointerString, IconFinal.c_str());                              //
+          nex.sendCommand(pointerString);                                           //Change the icon on screen
+          delete [] pointerString;                                                  //Delete the *char to free up mem
+          
+          IconFinal = "NexTemp.picc=" + String(i);
+          char *pointerStr = new char[  IconFinal.length() + 1];
+          strcpy(pointerStr, IconFinal.c_str());
+          nex.sendCommand(pointerStr);
+          delete [] pointerStr;
+          
+          IconFinal = "NexTimeSec.picc=" + String(i);
+          char *pointerSt = new char[  IconFinal.length() + 1];
+          strcpy(pointerSt, IconFinal.c_str());
+          nex.sendCommand(pointerSt);
+          delete [] pointerSt;
+          break;
+        }
+      }
+  }
    
  //Refresh nextion elements, otherwise background image (p0) will apear on top.    
   nex.sendCommand("ref p0");
   nex.sendCommand("ref NexTime");
+  nex.sendCommand("ref NexTimeSec");
   nex.sendCommand("ref NexMonth");
   nex.sendCommand("ref NexDate");
   nex.sendCommand("ref NexTemp");
@@ -126,55 +162,57 @@ void updateData() {
   nex.sendCommand("ref tagNight3");
   nex.sendCommand("ref NexTemp3");
 
-  nex.setComponentText("NexTemp", conditions.currentTemp);
-  Serial.println("CurrentTemp: " + conditions.currentTemp + "°C");
-  nex.setComponentText("NexHumid", conditions.humidity);
-  Serial.println("CurrentHumid: " + conditions.humidity);
-  nex.setComponentText("NexFLike", conditions.feelslike);
-  Serial.println("CurrentFeelsLike: " + conditions.feelslike);
-
-  String windSpeed = conditions.windSpeed;
-  windSpeed.remove(windSpeed.length() - 4);                                       //Remove 'km/h'
-  float windSpeedFloat = windSpeed.toFloat() * 0.277778;                          //Convert to m/s
-  float windSpeedFloatRounded = round(windSpeedFloat * 10) / 10.0;                //Round to the nearest tenth
-  windSpeed = String(windSpeedFloatRounded, 1) + "m/s";                           //Leave 1 nr after decimal
-  nex.setComponentText("NexWSpeed", windSpeed);
-  Serial.println("CurrentWindSpeed: " + windSpeed);
-
-  Serial.println("\nUpdating forecasts...");
-  String dayIcon;
-  String dayTitle;
-  String dayTemp;
-  wg.updateForecast(WG_API_KEY, WG_LANGUAGE, WG_COUNTRY, WG_CITY);                //Send HTTP request
-  for (int dayIndex = 0; dayIndex < 3; dayIndex++) {                              //Loop through forecast replys[day]
-    Serial.println("------------------------------------");
-    dayIcon = wg.getForecastIcon(dayIndex);
-    for (int i = 0; i < sizeof(dayIcons) / sizeof(dayIcons[0]); i++) {            //Loop through dayIcons array
-      if (dayIcon == dayIcons[i]) {
-        String dayIconFinal = "p" + String(dayIndex + 1) + ".pic=" + String(i + 16);  //forecast icons start from id 16 (on nextion)
-        char *pointerString = new char[dayIconFinal.length() + 1];                //Covert string to *char
-        strcpy(pointerString, dayIconFinal.c_str());                              //
-        nex.sendCommand(pointerString);                                           //Change the icon on screen
-        delete [] pointerString;                                                  //Delete the *char to free up mem
+  if (WiFi.status() == WL_CONNECTED) {
+    nex.setComponentText("NexTemp", conditions.currentTemp);
+    Serial.println("CurrentTemp: " + conditions.currentTemp + "°C");
+    nex.setComponentText("NexHumid", conditions.humidity);
+    Serial.println("CurrentHumid: " + conditions.humidity);
+    nex.setComponentText("NexFLike", conditions.feelslike);
+    Serial.println("CurrentFeelsLike: " + conditions.feelslike);
+  
+    String windSpeed = conditions.windSpeed;
+    windSpeed.remove(windSpeed.length() - 4);                                       //Remove 'km/h'
+    float windSpeedFloat = windSpeed.toFloat() * 0.277778;                          //Convert to m/s
+    float windSpeedFloatRounded = round(windSpeedFloat * 10) / 10.0;                //Round to the nearest tenth
+    windSpeed = String(windSpeedFloatRounded, 1) + "m/s";                           //Leave 1 nr after decimal
+    nex.setComponentText("NexWSpeed", windSpeed);
+    Serial.println("CurrentWindSpeed: " + windSpeed);
+  
+    Serial.println("\nUpdating forecasts...");
+    String dayIcon;
+    String dayTitle;
+    String dayTemp;
+    wg.updateForecast(WG_API_KEY, WG_LANGUAGE, WG_COUNTRY, WG_CITY);                //Send HTTP request
+    for (int dayIndex = 0; dayIndex < 3; dayIndex++) {                              //Loop through forecast replys[day]
+      Serial.println("------------------------------------");
+      dayIcon = wg.getForecastIcon(dayIndex);
+      for (int i = 0; i < sizeof(dayIcons) / sizeof(dayIcons[0]); i++) {            //Loop through dayIcons array
+        if (dayIcon == dayIcons[i]) {
+          String dayIconFinal = "p" + String(dayIndex + 1) + ".pic=" + String(i + 16);  //forecast icons start from id 16 (on nextion)
+          char *pointerString = new char[dayIconFinal.length() + 1];                //Covert string to *char
+          strcpy(pointerString, dayIconFinal.c_str());                              //
+          nex.sendCommand(pointerString);                                           //Change the icon on screen
+          delete [] pointerString;                                                  //Delete the *char to free up mem
+        }
       }
+      Serial.println("getForecastIcon: " + dayIcon);
+      dayTitle = wg.getForecastTitle(dayIndex);
+      nex.setComponentText("tagDay" + String(dayIndex + 1), dayTitle.substring(0, 3));  //Set only the first 3 characters
+      Serial.println("getForecastTitle: " + dayTitle);
+      if (dayTitle.endsWith("Night")) {                                             //If we're dealing with a night reply
+        nex.setComponentText("tagNight" + String(dayIndex + 1), "Night");
+        dayTemp = wg.getForecastText(dayIndex);                                 //Then get the day description for temperature
+        dayTemp = dayTemp.substring(dayTemp.indexOf("C.") - 3, dayTemp.indexOf("C."));      //Trim end: from 3 chars before C sign
+        dayTemp = dayTemp.substring(dayTemp.indexOf(" ") + 1);                               //Trim start
+      } else {                                                                      //If we're dealing with a day reply
+        nex.setComponentText("tagNight" + String(dayIndex + 1), "");                //Clear night tag
+        dayTemp = wg.getForecastLowTemp(dayIndex) + "|" + wg.getForecastHighTemp(dayIndex);
+      }
+      nex.setComponentText("NexTemp" + String(dayIndex + 1), dayTemp);
+      Serial.println("ForecastTemp: " + dayTemp);
+      //    Serial.println("getPoP: " + forecasts[i].PoP);
+  
     }
-    Serial.println("getForecastIcon: " + dayIcon);
-    dayTitle = wg.getForecastTitle(dayIndex);
-    nex.setComponentText("tagDay" + String(dayIndex + 1), dayTitle.substring(0, 3));  //Set only the first 3 characters
-    Serial.println("getForecastTitle: " + dayTitle);
-    if (dayTitle.endsWith("Night")) {                                             //If we're dealing with a night reply
-      nex.setComponentText("tagNight" + String(dayIndex + 1), "Night");
-      dayTemp = wg.getForecastText(dayIndex);                                 //Then get the day description for temperature
-      dayTemp = dayTemp.substring(dayTemp.indexOf("C.") - 3, dayTemp.indexOf("C."));      //Trim end: from 3 chars before C sign
-      dayTemp = dayTemp.substring(dayTemp.indexOf(" ") + 1);                               //Trim start
-    } else {                                                                      //If we're dealing with a day reply
-      nex.setComponentText("tagNight" + String(dayIndex + 1), "");                //Clear night tag
-      dayTemp = wg.getForecastLowTemp(dayIndex) + "|" + wg.getForecastHighTemp(dayIndex);
-    }
-    nex.setComponentText("NexTemp" + String(dayIndex + 1), dayTemp);
-    Serial.println("ForecastTemp: " + dayTemp);
-    //    Serial.println("getPoP: " + forecasts[i].PoP);
-
   }
   readyForWUpdate = false;
   delay(1000);
@@ -188,7 +226,14 @@ void updateClock() {
 
   //String time = timeClient.getFormattedTime().substring(0,5);
   nex.setComponentText("NexTime", timeClient.getFormattedTime().substring(0, 5));         //Cut out seconds
-
+  String secs;
+  if (timeClient.getSeconds() < 10) {
+    secs = "0" + String(timeClient.getSeconds());    
+  } else {
+    secs = timeClient.getSeconds();
+  }
+  nex.setComponentText("NexTimeSec", ":" + secs);
+  
   for (int i = 0; i < 12; i++) {                      //Loop through months
     if (timeClient.getMonth() == i + 1) {             //Add 1 becasue Month starts from 1
       nex.setComponentText("NexMonth", months[i]);
@@ -205,8 +250,24 @@ void updateClock() {
   readyForCUpdate = false;
 }
 
+void connectBg() {
+  Serial.println("\nConnecting...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  delay(25);
+  int tim = 0;
+  for (int i = 0; i < 20; i++) {
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("\n\nConnected");      
+      updateData();
+    }
+    delay(500);
+  }
+  readyforConnect = false;
+}
+
 void setGoodForWeatherUpdate() {
   readyForWUpdate = true;
+  Serial.println("\n\nreadyForWUpdate = tru");
 }
 
 void setGoodForClockUpdate() {
@@ -220,8 +281,8 @@ void setup() {
   connectWifi();
 
   updateData();
-  ticker.attach(UPDATE_INTERVAL_inSECS, setGoodForWeatherUpdate);       //Set a timer for updating weather info
-  ticker.attach(1, setGoodForClockUpdate);                              //Set a timer for updating clock  
+  ticker.attach(UPDATE_INTERVAL_inSECS, setGoodForWeatherUpdate);        //Set a timer for updating weather info
+  ticker1.attach(1, setGoodForClockUpdate);                              //Set a timer for updating clock  
 }
 
 void loop() {
@@ -230,6 +291,9 @@ void loop() {
   }
   if (readyForCUpdate) {
     updateClock();
+  }
+  if (readyforConnect) {
+    connectBg();
   }
   
   
